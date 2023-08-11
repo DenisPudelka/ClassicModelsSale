e713bbe3415c8711437c221be668c2f4bfba5205
DenisPudelka <denispudelka@gmail.com>
2023-08-11 17:50:42 +0200

    Repository: some methods needed to be implemented

--- src/main/java/com/example/classicmodlesslaes/repository/PaymentRepositoryImpl.java
+++ src/main/java/com/example/classicmodlesslaes/repository/PaymentRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.example.classicmodlesslaes.repository;
 
+import com.example.classicmodlesslaes.model.Customer;
 import com.example.classicmodlesslaes.model.Payment;
 import com.example.classicmodlesslaes.repository.interfaces.PaymentRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public class PaymentRepositoryImpl implements PaymentRepository {
@@ -21,5 +26,60 @@ public class PaymentRepositoryImpl implements PaymentRepository {
     public Payment getPaymentById(String id) {
         Payment payment = entityManager.find(Payment.class, id);
         return payment;
+    }
+
+    @Override
+    public void updatePayment(Payment payment) {
+
+    }
+
+    @Override
+    public void savePayment(Payment payment) {
+
+    }
+
+    @Override
+    public void deletePayment(String id) {
+
+    }
+
+    @Override
+    public List<Payment> getPaymentsByCustomerNumber(int customerNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getPaymentsAboveAmount(BigDecimal amount) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getTotalPaymentsByCustomer(int customerNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getPaymentsByDate(LocalDate paymentDate) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersWithPaymentsAbove(BigDecimal amount) {
+        return null;
+    }
+
+    @Override
+    public Payment getLastPaymentByCustomer(int customerNumber) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getTotalPaymentsInMonthYear(int month, int year) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersWithoutPayments() {
+        return null;
     }
 }
--- src/main/java/com/example/classicmodlesslaes/repository/interfaces/PaymentRepository.java
+++ src/main/java/com/example/classicmodlesslaes/repository/interfaces/PaymentRepository.java
@@ -1,7 +1,29 @@
 package com.example.classicmodlesslaes.repository.interfaces;
 
+import com.example.classicmodlesslaes.model.Customer;
 import com.example.classicmodlesslaes.model.Payment;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PaymentRepository {
+    // CRUD
     Payment getPaymentById(String id);
+    void updatePayment(Payment payment);
+    void savePayment(Payment payment);
+    void deletePayment(String id);
+
+    // Specific Queries
+    List<Payment> getPaymentsByCustomerNumber(int customerNumber);
+    List<Payment> getPaymentsAboveAmount(BigDecimal amount);
+    BigDecimal getTotalPaymentsByCustomer(int customerNumber);
+    List<Payment> getPaymentsByDate(LocalDate paymentDate);
+
+    // Extensions Beyond The Current Table
+    List<Customer> getCustomersWithPaymentsAbove(BigDecimal amount);
+    Payment getLastPaymentByCustomer(int customerNumber);
+    BigDecimal getTotalPaymentsInMonthYear(int month, int year);
+    List<Customer> getCustomersWithoutPayments();
+
 }
