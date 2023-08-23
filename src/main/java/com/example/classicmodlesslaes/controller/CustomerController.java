297c2cbad37554647a6b7d45b93f20b2ef8e6c6d
DenisPudelka <denispudelka@gmail.com>
2023-08-23 12:37:45 +0200

    Played around with some new thinks with controller

--- src/main/java/com/example/classicmodlesslaes/controller/CustomerController.java
+++ src/main/java/com/example/classicmodlesslaes/controller/CustomerController.java
@@ -1,20 +1,22 @@
 package com.example.classicmodlesslaes.controller;
 
 import com.example.classicmodlesslaes.dto.customer.CustomerBasicDTO;
+import com.example.classicmodlesslaes.dto.customer.CustomerDetailDTO;
 import com.example.classicmodlesslaes.dto.mappers.CustomerMapper;
 import com.example.classicmodlesslaes.model.Customer;
 import com.example.classicmodlesslaes.service.interfaces.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @RequestMapping("/classicmodelssales")
 public class CustomerController {
 
@@ -22,21 +24,25 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping("/customers")
-    public ResponseEntity<List<CustomerBasicDTO>> getAllCustomers(){
+    public String getAllCustomers(Model model){
         List<Customer> customers = customerService.getAllCustomers();
-        List<CustomerBasicDTO> customerBasicDTOS = new ArrayList<>();
-        for(Customer c : customers){
-            customerBasicDTOS.add(new CustomerMapper().toCustomerBasicDTO(c));
-        }
-        return ResponseEntity.ok(customerBasicDTOS);
+        List<CustomerBasicDTO> customerBasicDTOS = customers.stream()
+                .map(CustomerMapper::toCustomerBasicDTO)
+                .collect(Collectors.toList());
+        model.addAttribute("customers", customerBasicDTOS);
+        return "customers";
     }
+
     @GetMapping("/customer/{id}")
-    public ResponseEntity<CustomerBasicDTO> getCustomer(@PathVariable int id){
+    public String getCustomer(@PathVariable int id, Model model){
         Customer customer = customerService.getCustomerById(id);
         if(customer == null){
-            return ResponseEntity.notFound().build();
+            return "error";
         }
-        CustomerBasicDTO customerBasicDTO = new CustomerMapper().toCustomerBasicDTO(customer);
-        return ResponseEntity.ok(customerBasicDTO);
+        CustomerDetailDTO customerBasicDTO = new CustomerMapper().toCustomerDetailDTO(customer);
+        model.addAttribute("customerDetail", customer);
+        return "customer-detail";
     }
+
+
 }
---
+++ src/main/java/com/example/classicmodlesslaes/controller/MainPageController.java
@@ -0,0 +1,13 @@
+package com.example.classicmodlesslaes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/error")
+    public String showErrorPage() {
+        return "error"; // This maps to the error.html template
+    }
+}
