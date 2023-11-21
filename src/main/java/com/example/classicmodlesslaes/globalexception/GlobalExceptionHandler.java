a316e146145cb60348ce76ac583e56633a5aea74
Denis <denispudelka@gmail.com>
2023-11-21 10:18:48 +0100

    Implemented Global Exception Handler.

--- src/main/java/com/example/classicmodlesslaes/globalexception/GlobalExceptionHandler.java
+++ src/main/java/com/example/classicmodlesslaes/globalexception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.classicmodlesslaes.service.exceptions.DataAccessException;
 import com.example.classicmodlesslaes.service.exceptions.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,5 +24,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex){
         return new ResponseEntity<>("An error occured: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+/*
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMsg = ex.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+        return new ResponseEntity<>("Validation error: " + errorMsg, HttpStatus.BAD_REQUEST);
+    }
+
+ */
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>("Malformed JSON request: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
